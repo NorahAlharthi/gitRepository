a918367363b73151d1663d0d9a08b80d77b1b12e
922645 <922645@LTSA000373.cts.com>
2021-08-02 17:01:30 +0300

    changes 2

--- gitProject/src/gitProject/gitClass.java
+++ gitProject/src/gitProject/gitClass.java
@@ -5,4 +5,10 @@ int i;
 public void method1(){
 	System.out.println("method1");
 }
+public void method2(){
+	System.out.println("method2");
+}
+public void method3(){
+	System.out.println("method3");
+}
 }
